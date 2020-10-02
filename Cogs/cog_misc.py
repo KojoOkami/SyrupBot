1d90d97203825119927102945de4c4e36878210a
Nekomata5 <connormills535@gmail.com>
2020-10-02 15:40:26 -0700

    Fixed pong, set to hidden

--- Cogs/cog_misc.py
+++ Cogs/cog_misc.py
@@ -8,3 +8,8 @@ class Misc(commands.Cog, name='Miscellaneous'):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send('pong')
+
+    @commands.command(hidden=True)
+    async def pong(self, ctx):
+        if ctx.author.id == 688917542378537024:
+            await ctx.send(ctx.author.mention)
